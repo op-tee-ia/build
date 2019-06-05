e0258ba6dd320c15a5199c9d42a25fc085555463
Jingdong Lu <jingdong.lu@intel.com>
2019-06-05 12:38:21 +0800

    Fix compiling error and parallel compiling issue
    Signed-off-by: Jingdong Lu <jingdong.lu@intel.com>

--- bxt-p.mk
+++ bxt-p.mk
@@ -18,27 +18,29 @@ TOP_DIR = $(shell pwd)
 
 TOOLCHAIN_ROOT ?= $(TOP_DIR)/toolchain
 GCC_TOOLCHAIN_PATH := $(TOOLCHAIN_ROOT)/gcc/x86_64-linux-android-4.9/bin
-CROSS_COMPILE=$(GCC_TOOLCHAIN_PATH)/x86_64-linux-android-
+TA_DEV_KIT_ROOT := $(TOP_DIR)/optee_os
+OPTEE_CLIENT_EXPORT_ROOT := $(TOP_DIR)/optee_client
 
 OPTEE_OS_ENV_VAR += ARCH=x86_64
+OPTEE_OS_ENV_VAR += CROSS_COMPILE=$(GCC_TOOLCHAIN_PATH)/x86_64-linux-android-
 OPTEE_OS_ENV_VAR += CROSS_COMPILE64=$(GCC_TOOLCHAIN_PATH)/x86_64-linux-android-
 OPTEE_OS_ENV_VAR += PLATFORM=bxtp
 OPTEE_OS_ENV_VAR += O=$(TOP_DIR)/out/optee_os
 OPTEE_OS_ENV_VAR += CFG_TEE_BENCHMARK=n
+OPTEE_OS_ENV_VAR += CFG_TEE_CORE_LOG_LEVEL=3
+OPTEE_OS_ENV_VAR += DEBUG=1
 
+OPTEE_CLIENT_ENV_VAR += CROSS_COMPILE=
 OPTEE_CLIENT_ENV_VAR += O=$(TOP_DIR)/out/optee_client
 OPTEE_CLIENT_ENV_VAR += CFG_TEE_CLIENT_LOAD_PATH=/data/tee
 
-OPTEE_TEST_ENV_VAR += TA_DEV_KIT_ROOT=$(TOP_DIR)/optee_os
-OPTEE_TEST_ENV_VAR += OPTEE_CLIENT_EXPORT_ROOT=$(TOP_DIR)/optee_client
-OPTEE_TEST_ENV_VAR += TA_DEV_KIT_DIR=${TA_DEV_KIT_ROOT}/out/x86_64-plat-bxtp/export-ta_x86_64
-OPTEE_TEST_ENV_VAR += OPTEE_CLIENT_EXPORT=${OPTEE_CLIENT_EXPORT_ROOT}/out/export/usr
+OPTEE_TEST_ENV_VAR += TA_DEV_KIT_DIR=${TOP_DIR}/out/optee_os/export-ta_x86_64
+OPTEE_TEST_ENV_VAR += OPTEE_CLIENT_EXPORT=${TOP_DIR}/out/optee_client/export/usr
 OPTEE_TEST_ENV_VAR += O=$(TOP_DIR)/out/optee_test
 OPTEE_TEST_ENV_VAR += TA_DIR=/data/tee/optee_armtz
 
-IKGT_ENV_VAR += COMPILE_TOOLCHAIN=$(GCC_TOOLCHAIN_PATH)/x86_64-linux-android-
 IKGT_ENV_VAR += BUILD_DIR=$(TOP_DIR)/out/ikgt/
-IKGT_ENV_VAR += OPTEEBIN_DIR=$(TOP_DIR)/optee_os/out/x86_64-plat-bxtp/core/
+IKGT_ENV_VAR += OPTEEBIN_DIR=$(TOP_DIR)/out/optee_os/core/
 IKGT_ENV_VAR += BOOT_ARCH=abl
 IKGT_ENV_VAR += TRUSTY_REF_TARGET=optee_gordon_peak
 
@@ -59,13 +61,13 @@ optee_client:
 	@echo '****************************************************************'
 	$(OPTEE_CLIENT_ENV_VAR) $(MAKE) -C optee_client
 
-optee_test:
+optee_test: optee_client optee_os
 	@echo '****************************************************************'
 	@echo '*   build optee_test ...'
 	@echo '****************************************************************'
 	$(OPTEE_TEST_ENV_VAR) $(MAKE) -C optee_test
 
-ikgt:
+ikgt: optee_os
 	@echo '****************************************************************'
 	@echo '*   build ikgt core...'
 	@echo '****************************************************************'
